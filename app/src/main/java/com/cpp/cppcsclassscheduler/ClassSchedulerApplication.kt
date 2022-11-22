1a30362d4c8a77e0702f9565108287fd6ba2f792
Luke Dinh <lukexdinh@gmail.com>
2022-11-22 11:35:52 -0800

    feat(database): added Room database, DAO, and repository with CPP CS classes, and repository with CPP CS classes

---
+++ app/src/main/java/com/cpp/cppcsclassscheduler/ClassSchedulerApplication.kt
@@ -0,0 +1,11 @@
+package com.cpp.cppcsclassscheduler
+
+import android.app.Application
+
+class ClassSchedulerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        CsClassRepository.initialize(this)
+    }
+}
---
+++ app/src/main/java/com/cpp/cppcsclassscheduler/database/CsClassDatabase.kt
@@ -0,0 +1,10 @@
+package com.cpp.cppcsclassscheduler.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.cpp.cppcsclassscheduler.CsClass
+
+@Database(entities = [CsClass::class], version = 1)
+abstract class CsClassDatabase : RoomDatabase() {
+    abstract fun CsClassDao(): CsClassDao
+}
