867cdfbc81c5654d4b8bd7ec484c2e2f1abb2496
Taylor Vandenberg <36976156+TaylorVandenberg@users.noreply.github.com>
2022-11-29 14:35:10 -0800

    Add files via upload

--- app/src/main/java/com/cpp/cppcsclassscheduler/MainActivity.kt
+++ app/src/main/java/com/cpp/cppcsclassscheduler/MainActivity.kt
@@ -1,21 +1,76 @@
 package com.cpp.cppcsclassscheduler
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
 
 private const val TAG = "MainActivity"
+private const val RC_SIGN_IN = 123
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account != null){
+            val intent = Intent(this, ClassListView::class.java)
+            startActivity(intent)
+        }
+
+        val signInButton = findViewById<Button>(R.id.button)
+        signInButton.setOnClickListener {
+            val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
         val skipButton = findViewById<Button>(R.id.button2)
         skipButton.setOnClickListener {
             val intent = Intent(this, ClassListView::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            val intent = Intent(this, ClassListView::class.java)
+            startActivity(intent)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
         }
     }
 }
