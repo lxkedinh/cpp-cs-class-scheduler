18c58759e070f417ebbc8a7508cc96f2bd2029ea
Luke Dinh <lukexdinh@gmail.com>
2022-11-29 16:43:35 -0800

    chore(UI): saving current progress

--- app/src/main/java/com/cpp/cppcsclassscheduler/MainActivity.kt
+++ app/src/main/java/com/cpp/cppcsclassscheduler/MainActivity.kt
@@ -3,19 +3,36 @@ package com.cpp.cppcsclassscheduler
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import java.util.concurrent.Executors
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private val repository = CsClassRepository.get()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val skipButton = findViewById<Button>(R.id.button2)
         skipButton.setOnClickListener {
-            val intent = Intent(this, ClassListView::class.java)
+            val intent = Intent(this, ClassSearchActivity::class.java)
             startActivity(intent)
         }
+
+        val executor = Executors.newSingleThreadExecutor()
+        var classes: List<CsClass>
+        executor.execute {
+            classes = repository.searchClasses("python")
+            Log.d(TAG, classes.size.toString())
+            for (csClass in classes) {
+                Log.d(TAG, csClass.name)
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/cpp/cppcsclassscheduler/database/CsClassDao.kt
+++ app/src/main/java/com/cpp/cppcsclassscheduler/database/CsClassDao.kt
@@ -13,6 +13,13 @@ interface CsClassDao {
     @Query("SELECT * FROM csclass WHERE id = :id")
     fun getAllSections(id: Int): List<CsClass>
 
+    @Query("""
+        SELECT *
+        FROM csClasses_fts
+        WHERE csClasses_fts MATCH :query
+    """)
+    fun searchClasses(query: String?): List<CsClassFTS>
+
     @Insert
     fun addClasses(classes: List<CsClass>)
 }
